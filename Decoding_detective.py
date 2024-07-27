a8c2ca9fcede2635e5cc383c099fe1eb07150494
Daniel Sousa <sousadanielsb@gmail.com>
2024-07-27 16:02:55 -0300

    Rascunho

--- Decoding_detective.py
+++ Decoding_detective.py
@@ -1,5 +1,14 @@
 import random as rd
+from huffman import compressao, descompressao
 
 WORDS = ["decoding", "detective", "huffman"]
 # WORD_SELECTED = rd.choice(WORDS)
 WORD_SELECTED = "decoding"
+
+word_codificada, codigos = compressao(WORD_SELECTED)
+word_descodificada = descompressao(word_codificada, codigos)
+
+print(f"A palavra é {WORD_SELECTED}")
+print(f"Texto Codificado: {word_codificada}")
+print(f"Códigos: {codigos}")
+print(f"Texto descodificado: {word_descodificada}")
